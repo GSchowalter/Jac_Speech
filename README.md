4d1a08045a52f03e91da74879f69ec67045443fa
Grant Schowalter <grantschowalter@gmail.com>
2021-03-21 21:43:24 -0400

    Added gitignore and set up environment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Jac_Speech
 Jac Speech is the voice interaction interfaced used on the JAC Cart autonomous vehicle research project being conducted at JMU.
+
+# Dependencies and Installations
+
+## Deep speech
+- Mozilla Deep speech has a pretrained model that is used for voice recognition in this project
+- When setting up the installation execute the following commands in a new python virtual environment
+`pip install deepspeech`
+`pip install -r requirements`
--- speech_interface.py
+++ speech_interface.py
@@ -1,10 +1,59 @@
-import socket               # Import socket module
+import socket
+
+import numpy as np               # Import socket module
+from mic_vad_streaming import VADAudio
+import os
+import logging
+import numpy as np
+import datetime as datetime
 
 sock = socket.socket()         # Create a socket object
 host = '127.0.0.1' # Get local machine name
-port = 2500
-print(host)                # Reserve a port for your service.
-
+port = 2500             # Reserve a port for your service.
 sock.connect((host, port))
 print(sock.recv(1024))
-sock.close()                     # Close the socket when done
+sock.close()                    # Close the socket when done
+
+def main(ARGS):
+    # Load DeepSpeech model
+    if os.path.isdir(ARGS.model):
+        model_dir = ARGS.model
+        ARGS.model = os.path.join(model_dir, 'output_graph.pb')
+        ARGS.scorer = os.path.join(model_dir, ARGS.scorer)
+
+    print('Initializing model...')
+    logging.info("ARGS.model: %s", ARGS.model)
+    model = deepspeech.Model(ARGS.model)
+    if ARGS.scorer:
+        logging.info("ARGS.scorer: %s", ARGS.scorer)
+        model.enableExternalScorer(ARGS.scorer)
+
+    # Start audio with VAD
+    vad_audio = VADAudio(aggressiveness=ARGS.vad_aggressiveness,
+                         device=ARGS.device,
+                         input_rate=ARGS.rate,
+                         file=ARGS.file)
+    print("Listening (ctrl-C to exit)...")
+    frames = vad_audio.vad_collector()
+
+    # Stream from microphone to DeepSpeech using VAD
+    spinner = None
+    if not ARGS.nospinner:
+        spinner = Halo(spinner='line')
+    stream_context = model.createStream()
+    wav_data = bytearray()
+    for frame in frames:
+        if frame is not None:
+            if spinner: spinner.start()
+            logging.debug("streaming frame")
+            stream_context.feedAudioContent(np.frombuffer(frame, np.int16))
+            if ARGS.savewav: wav_data.extend(frame)
+        else:
+            if spinner: spinner.stop()
+            logging.debug("end utterence")
+            if ARGS.savewav:
+                vad_audio.write_wav(os.path.join(ARGS.savewav, datetime.now().strftime("savewav_%Y-%m-%d_%H-%M-%S_%f.wav")), wav_data)
+                wav_data = bytearray()
+            text = stream_context.finishStream()
+            print("Recognized: %s" % text)
+            stream_context = model.createStream()
